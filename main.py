06cfd78324427f9ea041f235a1fd8cf955c4f682
Mohamed Mubeen <mubeensgh@gmail.com>
2025-05-18 09:05:18 +0800

    added rotation to player

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.update(dt)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
