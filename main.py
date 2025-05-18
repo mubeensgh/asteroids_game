4491c781183aae2b79726bf856eef4f9efc5299c
Mohamed Mubeen <mubeensgh@gmail.com>
2025-05-18 14:50:02 +0800

    added forward and backwards movement

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
         player.update(dt)
+        pygame.display.flip()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
